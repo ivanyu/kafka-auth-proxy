8267f690e9ec135a5b67712fb5f6dcd6223a028f
Ivan Yurchenko <ivan@ivanyu.me>
2024-06-04 20:22:06 +0300

    WIP

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,12 @@
-use std::hint::black_box;
 use std::io::ErrorKind::UnexpectedEof;
 use std::mem::size_of;
-use tokio::io::{self, AsyncReadExt, AsyncWriteExt, BufReader, BufWriter, stdout};
+use tokio::io::{ AsyncReadExt, AsyncWriteExt, BufReader, BufWriter};
 use tokio::net::{TcpListener, TcpStream};
+use tokio::net::tcp::{ReadHalf, WriteHalf};
+use std::io::Result;
 
 #[tokio::main]
-async fn main() -> tokio::io::Result<()> {
+async fn main() -> Result<()> {
     let listener = TcpListener::bind("127.0.0.1:10000").await.unwrap();
     loop {
         let (socket, _) = listener.accept().await.unwrap();
@@ -13,8 +14,6 @@ async fn main() -> tokio::io::Result<()> {
             process(socket).await;
         });
     }
-
-    Ok(())
 }
 
 
@@ -22,20 +21,30 @@ async fn process(mut client_socket: TcpStream) {
     client_socket.set_nodelay(true).unwrap();
     // client_socket.readable().await.unwrap();
     // client_socket.writable().await.unwrap();
-    let (mut client_rd, mut client_wr) = client_socket.split();
+    let (client_rd, client_wr) = client_socket.split();
     let mut client_rd_buf = BufReader::new(client_rd);
     let mut client_wr_buf = BufWriter::new(client_wr);
 
     let mut broker_socket = TcpStream::connect("127.0.0.1:9092").await.unwrap();
     // broker_socket.readable().await.unwrap();
     // broker_socket.writable().await.unwrap();
-    let (mut broker_rd, mut broker_wr) = broker_socket.split();
+    let (broker_rd, broker_wr) = broker_socket.split();
     let mut broker_rd_buf = BufReader::new(broker_rd);
     let mut broker_wr_buf = BufWriter::new(broker_wr);
 
     loop {
-        let request_size = match client_rd_buf.read_i32().await {
-            Ok(s) => s,
+        let r = tokio::select! {
+            request_size_res = client_rd_buf.read_i32() => {
+                process_request(&mut client_rd_buf, &mut broker_wr_buf, request_size_res).await
+            }
+
+            response_size_res = broker_rd_buf.read_i32() => {
+                process_response(&mut client_wr_buf, &mut broker_rd_buf, response_size_res).await
+            }
+        };
+
+        match r {
+            Ok(()) => {},
 
             Err(e) if e.kind() == UnexpectedEof => {
                 client_socket.shutdown().await.unwrap();
@@ -50,50 +59,52 @@ async fn process(mut client_socket: TcpStream) {
                 return;
             }
         };
-
-        let request_api_key = client_rd_buf.read_i16().await.unwrap();
-        let request_api_version = client_rd_buf.read_i16().await.unwrap();
-        let correlation_id = client_rd_buf.read_i32().await.unwrap();
-
-        println!("Request API key: {}, version: {}, correlation ID: {}",
-                 request_api_key, request_api_version, correlation_id);
-
-        let buf_size = request_size as usize - size_of::<i16>() - size_of::<i16>() - size_of::<i32>();
-        let mut buf = vec![0; buf_size];
-        client_rd_buf.read_exact(&mut buf).await.unwrap();
-
-        broker_wr_buf.write_i32(request_size).await.unwrap();
-        broker_wr_buf.write_i16(request_api_key).await.unwrap();
-        broker_wr_buf.write_i16(request_api_version).await.unwrap();
-        broker_wr_buf.write_i32(correlation_id).await.unwrap();
-        broker_wr_buf.write(&buf).await.unwrap();
-        broker_wr_buf.flush().await.unwrap();
-
-        println!("Request proxied");
-
-        let response_size = broker_rd_buf.read_i32().await.unwrap();
-        // let correlation_id = client_rd_buf.read_i32().await.unwrap();
-
-        // println!("Response correlation ID: {}",
-        //          correlation_id);
-
-        // let buf_size = response_size as usize - size_of::<i32>();
-        let buf_size = response_size as usize;
-        let mut buf = vec![0; buf_size];
-        broker_rd_buf.read_exact(&mut buf).await.unwrap();
-        client_wr_buf.write_i32(response_size).await.unwrap();
-        // client_wr_buf.write_i32(correlation_id).await.unwrap();
-        client_wr_buf.write(&buf).await.unwrap();
-        client_wr_buf.flush().await.unwrap();
-
-        println!("Response proxied")
     }
 }
 
-async fn process_from_client_to_broker() {
+async fn process_request(client_rd_buf: &mut BufReader<ReadHalf<'_>>,
+                         broker_wr_buf: &mut BufWriter<WriteHalf<'_>>,
+                         request_size_res: Result<i32>) -> Result<()> {
+    let request_size = request_size_res?;
+    let request_api_key = client_rd_buf.read_i16().await?;
+    let request_api_version = client_rd_buf.read_i16().await?;
+    let correlation_id = client_rd_buf.read_i32().await?;
 
+    println!("Request API key: {}, version: {}, correlation ID: {}",
+             request_api_key, request_api_version, correlation_id);
+
+    let buf_size = request_size as usize - size_of::<i16>() - size_of::<i16>() - size_of::<i32>();
+    let mut buf = vec![0; buf_size];
+    client_rd_buf.read_exact(&mut buf).await?;
+
+    broker_wr_buf.write_i32(request_size).await?;
+    broker_wr_buf.write_i16(request_api_key).await?;
+    broker_wr_buf.write_i16(request_api_version).await?;
+    broker_wr_buf.write_i32(correlation_id).await?;
+    broker_wr_buf.write(&buf).await?;
+    broker_wr_buf.flush().await?;
+
+    println!("Request proxied");
+    Ok(())
 }
 
-async fn process_from_broker_to_client() {
+async fn process_response(client_wr_buf: &mut BufWriter<WriteHalf<'_>>,
+                          broker_rd_buf: &mut BufReader<ReadHalf<'_>>,
+                          response_size_res: Result<i32>) -> Result<()> {
+    let response_size = response_size_res?;
+    let correlation_id = broker_rd_buf.read_i32().await?;
 
+    println!("Response correlation ID: {}",
+             correlation_id);
+
+    let buf_size = response_size as usize - size_of::<i32>();
+    let mut buf = vec![0; buf_size];
+    broker_rd_buf.read_exact(&mut buf).await?;
+    client_wr_buf.write_i32(response_size).await?;
+    client_wr_buf.write_i32(correlation_id).await?;
+    client_wr_buf.write(&buf).await?;
+    client_wr_buf.flush().await?;
+
+    println!("Response proxied");
+    Ok(())
 }
